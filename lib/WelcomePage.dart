f2380cb81a6a87f5a4d3c977f2a57be0db2ddfe8
Nabeedjamshed <nabeedjamshedali800@gmail.com>
2024-10-21 23:38:23 +0500

    Modify welcome page

--- lib/WelcomePage.dart
+++ lib/WelcomePage.dart
@@ -11,80 +11,63 @@ class Welcomepage extends StatefulWidget {
 }
 
 class _ManageState extends State<Welcomepage> {
-  double? _deviceWidth;
-  double? _deviceHeight;
   @override
   Widget build(BuildContext context) {
-    _deviceWidth = MediaQuery.of(context).size.width;
-    _deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.grey[300], // Use plain color if you remove image
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: const Text("Welcome",
             style: TextStyle(fontWeight: FontWeight.w500)),
         leading: const Icon(Icons.home),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(2),
-        child: Stack(
-          children: [
-            Image.asset(
-              "Assets/whitebakcground.png",
-              width: _deviceWidth,
-              height: _deviceHeight! * 0.8,
-              fit: BoxFit.fill,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: Image.asset(
+              "Assets/avatar.png",
+              width: MediaQuery.of(context).size.width * 0.6,
+              alignment: Alignment.center,
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Center(
-                  child: Image.asset(
-                    "Assets/avatar.png",
-                    width: _deviceWidth! * 0.6,
-                    alignment: Alignment.center,
-                  ),
+          ),
+          const Column(
+            children: [
+              Text(
+                "Welcome",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Find nearby service provider",
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 250,
+            child: MaterialButton(
+              color: const Color.fromARGB(255, 35, 247, 130),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return const Accounttype();
+                }));
+              },
+              height: 35,
+              child: const Text(
+                "Get Started",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
                 ),
-                const Column(
-                  children: [
-                    Text(
-                      "Welcome",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Find nearby service provider",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: 250,
-                  child: MaterialButton(
-                    color: const Color.fromARGB(255, 35, 247, 130),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    onPressed: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) {
-                        return const Accounttype();
-                      }));
-                    },
-                    height: 35,
-                    child: const Text(
-                      "Get Started",
-                      style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
